0f7902f8e598c5dd4bbe2839356a32fa9816d624
BaptisteV <86929180+MadBrosos@users.noreply.github.com>
2022-09-14 11:25:58 +0200

    Error Incoming

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Petite Erreur
     }
 
     // Update is called once per frame
